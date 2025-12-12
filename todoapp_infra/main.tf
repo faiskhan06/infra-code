735c94bc08ef4ec47534ecdf82e6dccd53aa0ea7
faiskhan06 <faisalec06@gmail.com>
2025-12-12 12:10:39 +0530

    new resource added

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -1,11 +1,16 @@
 module "resource_group" {
   source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "rg-todoapp"
-  resource_group_location = "centralindia"
+  resource_group_name     = "rg-us"
+  resource_group_location = "eastus"
 }
 
 module "resource_group1" {
   source                  = "../modules/azurerm_resource_group"
   resource_group_name     = "rg-india"
   resource_group_location = "centralindia"
+}
+module "resource_group1" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-canada"
+  resource_group_location = "canadacentral"
 }
